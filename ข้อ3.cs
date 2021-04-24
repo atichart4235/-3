12b566b25e8b883a30aad97191d5a8a18e09f940
atichart4235 <chayanon.bunprasert@mail.kmutt.ac.th>
2021-04-24 16:09:44 +0700

    ข้อ3

---
+++ ข้อ3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp54
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mode = Console.ReadLine();
+            float filmBackWidth = float.Parse(Console.ReadLine());
+            while (filmBackWidth <= 0)
+            {
+                Console.WriteLine("Invalid filmBackWidth.Please input again.");
+                filmBackWidth = float.Parse(Console.ReadLine());
+            }
+            float fLength, fov;
+            if (mode == "fLength")
+            {
+                fLength = float.Parse(Console.ReadLine());
+                while (fLength <=0)
+                {
+                    Console.WriteLine("Invalid fLength.Please input again.");
+                    fLength = float.Parse(Console.ReadLine());
+                }
+                fov = 2 * Math.Atan(double.Parse(filmBackWidth) / 2);
+            }
+        }
+    }
+}
